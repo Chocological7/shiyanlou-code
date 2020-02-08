a68e91e11118323d11d59636e7ea3739fd5ee8a6
Chocological7 <1044049571@qq.com>
2020-02-08 12:23:38 +0800

    basic code

---
+++ jump7.py
@@ -0,0 +1,28 @@
+mkdir Demo
+cd Demo
+gedit READEME.md
+
+cat READEME.md
+git init
+ls -la
+
+cd /home/shiyanlou/Demo
+git add READEME.md
+git status
+git config --global user.name "Chocological7"
+git config --global user.email "1044049571@qq.com"
+git commit -m"first commit"
+
+git remote add origin ????
+git push origin master
+//???????
+
+cd /home/shiyanlou/
+git clone https://github.com/shiyanlou/gitproject
+
+
+git clone https://github.com/Chocological7/Louplus.git
+wget https://labfile.oss.aliyuncs.com/courses/1330/linux.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python1.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python2.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/git.png
